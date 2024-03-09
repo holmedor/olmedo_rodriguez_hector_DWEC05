cc798408b5a5cfcccbae32c7c8b6f366f6a4f9ce
HÉCTOR OLMEDO <holmedo@birt.eus>
2024-03-09 11:09:06 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'olmedo_rodriguez_hector_DWEC05';
+}
